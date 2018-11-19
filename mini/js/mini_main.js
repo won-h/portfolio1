5b5745b2cc0c48f634a2fa94cb3efef9c6edcb8d
won-h <45153194+won-h@users.noreply.github.com>
2018-11-19 09:53:37 +0900

    Update mini_main.js

--- mini/js/mini_main.js
+++ mini/js/mini_main.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
-	$('#loading').delay(3000).fadeOut();
+	$('#loading').addClass('on').fadeIn(function(){
+		$('html, body').css({'overflow': 'hidden', 'height': '100%'});
+		$('body').on('scroll touchmove mousewheel', function(event) {
+			event.preventDefault();
+			event.stopPropagation();
+		return false;
+		});
+	});
+
+	$('#loading').delay(3000).fadeOut(function(){
+		$(this).removeClass('on')
+		$('body').off('scroll touchmove mousewheel');
+	});
 });
+
